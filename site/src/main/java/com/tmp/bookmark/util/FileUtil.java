5db5de1d7cbdfa2081ef6cd799acda38abda18d9
Andrei Apostol <kamerad_andrei@hotmail.com>
2013-02-14 16:44:27 -0500

    FileUtil added createand Write to file

--- site/src/main/java/com/tmp/bookmark/util/FileUtil.java
+++ site/src/main/java/com/tmp/bookmark/util/FileUtil.java
@@ -1,7 +1,6 @@
 package com.tmp.bookmark.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -31,6 +30,28 @@ public class FileUtil {
         scanner.close();
 
         return contents;
+    }
+
+    public boolean createAndWriteToFile(String dirPath, String filename, String contents) throws IOException {
+
+        File tempfile = new File(dirPath, filename);
+        FileWriter fw;
+
+        // if file doesnt exist, then create it
+        if (!tempfile.exists()) {
+            tempfile.createNewFile();
+            fw = new FileWriter(tempfile.getAbsoluteFile());
+        }
+        else{
+            // append to file, instead of overwriting
+            fw = new FileWriter(tempfile.getAbsoluteFile(), true);
+        }
+
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(contents);
+        bw.close();
+
+        return true;
     }
 
 
