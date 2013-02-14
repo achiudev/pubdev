ccddf9b9f8b6232e642a0335a838238c99f01fab
Andrei Apostol <kamerad_andrei@hotmail.com>
2013-02-14 10:49:54 -0500

    JSON code

--- site/src/test/java/JDBCTester.java
+++ site/src/test/java/JDBCTester.java
@@ -2,10 +2,14 @@ import com.tmp.bookmark.config.ConnectionManager;
 import com.tmp.bookmark.di.CustomerDataInterface;
 import com.tmp.bookmark.model.Customer;
 import com.tmp.bookmark.model.CustomerBookmark;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.Vector;
 
 
@@ -23,7 +27,7 @@ public class JDBCTester {
             Vector<Customer> customers = new Vector<Customer>();
             CustomerBookmark cust_bookmark = null;
 
-            //  Connection con = DriverManager.getConnection(dbUrl, "monty", "some_pass");
+            //  Connection con = DriverManager.getConnection(dbUrl, "monty", "faaaaaaake");
             Connection con = ConnectionManager.getInstance().getConnection();
 
           /*  Customer customer = CustomerDataInterface.getInstance().getCustomerByID(con, "ABCD1234");
@@ -37,13 +41,13 @@ public class JDBCTester {
             cust_bookmark = CustomerDataInterface.getInstance().getBookmarkByCustomerID(con, "ABCD1234");
             System.out.println(cust_bookmark.getCust_id()+"||"+cust_bookmark.getBookmark_location());
 
-           /* Scanner scanner =
-                    new Scanner(new File(cust_bookmark.getBookmark_location())).useDelimiter("\\Z");
+            Scanner scanner =
+                    new Scanner(new File("C:\\json_andrei.txt")).useDelimiter("\\Z");
             String contents = scanner.next();
             System.out.println(contents);
-            scanner.close();   */
+            scanner.close();
 
-            InputStream in = JDBCTester.class.getClassLoader().getResourceAsStream("resources/json_andrei.txt");
+         /*   InputStream in = JDBCTester.class.getClassLoader().getResourceAsStream("resources/json_andrei.txt");
             try {
                 BufferedReader reader=new BufferedReader(new InputStreamReader(in));
                 String line=null;
@@ -53,13 +57,27 @@ public class JDBCTester {
             } catch (Exception e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-            }
+            }                */
 
             con.close();
+
+           JSONObject j_obj = new JSONObject(contents);
+            JSONArray employees = j_obj.getJSONArray("bookmarks");
+
+            for (int i = 0 ; i < employees.length(); i++){
+                System.out.println(employees.getJSONObject(i).get("description")+"--->"+employees.getJSONObject(i).get("link"));
+            }
+
+
+
         } //end try
 
         catch (SQLException e) {
-            e.printStackTrace();  }
+            e.printStackTrace();  } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
 
 
     } //end main
