e89b89c47c4382e7f3c63999abba3800c3a24e60
Andrei Apostol <kamerad_andrei@hotmail.com>
2013-02-15 16:21:47 -0500

    added Constants class

---
+++ site/src/main/java/com/tmp/bookmark/util/Constants.java
@@ -0,0 +1,15 @@
+package com.tmp.bookmark.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aapostol
+ * Date: 15/02/13
+ * Time: 4:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constants {
+
+    public static final String jsonDir = "json_files";
+
+
+}
