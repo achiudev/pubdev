c9d85f2c9980fe6d0ef414d4c24b47eb75953329
Andrei Apostol <kamerad_andrei@hotmail.com>
2013-02-15 15:21:46 -0500

    added ON DELETE CASCADE

--- site/src/main/resources/sql/alex.sql
+++ site/src/main/resources/sql/alex.sql
@@ -2,4 +2,4 @@ GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' IDENTIFIED BY 'some_pass' WITH
 
 create table customer (cust_id char(20) PRIMARY KEY, first_name char(20), last_name char(20), country char(20));
 
-create table cust_bookmark (cust_id char (20), bookmark_location varchar(300), FOREIGN KEY (cust_id) REFERENCES customer(cust_id));
+create table cust_bookmark (cust_id char (20), bookmark_location varchar(300), FOREIGN KEY (cust_id) REFERENCES customer(cust_id) ON DELETE CASCADE);
