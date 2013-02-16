b8160f6f349abfce8c3413a11a939fd32f7ee874
Alex Chiu <achiudev@gmail.com>
2013-02-16 18:02:25 -0500

    Removing target

--- site/target/classes/sql/startup_tables.sql
+++
@@ -1,8 +0,0 @@
---Create User table
-create table customer (cust_id char(20) PRIMARY KEY, first_name char(20), last_name char(20), country char(20));
-insert into customer VALUES ('ABCD1234', 'Andrei', 'Apostol', 'CA');
-insert into customer VALUES ('WXYZ4567', 'Alex', 'Chiu', 'CA');
-
---Create bookmark table
-create table cust_bookmark (cust_id char (20), bookmark_location varchar(300), FOREIGN KEY (cust_id) REFERENCES customer(cust_id));
-insert into cust_bookmark values ('ABCD1234', 'json_files/json.txt');
